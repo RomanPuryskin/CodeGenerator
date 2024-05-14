fb2c4fcc979166128ad71895d90dd1ef1d01c5a1
Roman <rputyskin@mail.ru>
2024-05-14 09:16:00 +0700

    Обращение к тестовому классу через указатель

--- CodeGenerator/main.cpp
+++ CodeGenerator/main.cpp
@@ -9,7 +9,6 @@ int main() {
                  "3 for C#\n" <<std::endl;
     unsigned int classControl;
     std::cin>>classControl;
-    //ClassGeneration *generate = new ClassGeneration(classControl);
     auto generate = std::make_shared<ClassGeneration>(classControl);
     std::cout<<generate->Program();
     return 0;
