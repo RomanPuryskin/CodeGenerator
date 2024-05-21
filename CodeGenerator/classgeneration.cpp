556d38eba3025ea64b59c6bb2acfb0eb806f640f
Roman <rputyskin@mail.ru>
2024-05-21 14:45:48 +0700

    generateProgram через m_factory

--- CodeGenerator/classgeneration.cpp
+++ CodeGenerator/classgeneration.cpp
@@ -20,19 +20,19 @@ ClassGeneration::ClassGeneration( unsigned int classFlag)
     m_flag = classFlag;
 }
 
-std::string ClassGeneration::generateProgramCpp( const std::shared_ptr< ICodeGenerator >& factory )
+std::string ClassGeneration::generateProgramCpp()
 {
-    auto myClass = factory->addClass( "MyClass" );
-    myClass->add( factory->addMethod( "testFunc1", "void", 0 ), ClassUnit::PUBLIC);
-    myClass->add( factory->addMethod( "testFunc2", "void", MethodUnit::STATIC ), ClassUnit::PRIVATE);
-    myClass->add( factory->addMethod( "testFunc3", "void", MethodUnit::VIRTUAL | MethodUnit::CONST ), ClassUnit::PUBLIC);
-    std::shared_ptr< MethodUnit > method = factory->addMethod( "testFunc4", "void", MethodUnit::STATIC );
-    method->add( factory->addOperator( R"(Hello, world!\n)" ) );
+    auto myClass = m_factory->addClass( "MyClass" );
+    myClass->add( m_factory->addMethod( "testFunc1", "void", 0 ), ClassUnit::PUBLIC);
+    myClass->add( m_factory->addMethod( "testFunc2", "void", MethodUnit::STATIC ), ClassUnit::PRIVATE);
+    myClass->add( m_factory->addMethod( "testFunc3", "void", MethodUnit::VIRTUAL | MethodUnit::CONST ), ClassUnit::PUBLIC);
+    std::shared_ptr< MethodUnit > method = m_factory->addMethod( "testFunc4", "void", MethodUnit::STATIC );
+    method->add( m_factory->addOperator( R"(Hello, world!\n)" ) );
     myClass->add( method, ClassUnit::PROTECTED );
     return myClass->compile();
 }
-std::string ClassGeneration::generateProgramJava( const std::shared_ptr< ICodeGenerator >& factory ) {
-    auto myClass = factory->addClass( "MyClass" );
+std::string ClassGeneration::generateProgramJava() {
+    auto myClass = m_factory->addClass( "MyClass" );
     myClass->add( m_factory->addMethod( "testFunc1", "void", 0 ), ClassUnit::PUBLIC);
     myClass->add( m_factory->addMethod( "testFunc2", "void", MethodUnit::STATIC ), ClassUnit::PRIVATE);
     myClass->add( m_factory->addMethod( "testFunc3", "void", MethodUnit::ABSTRACT), ClassUnit::PUBLIC);
@@ -41,8 +41,8 @@ std::string ClassGeneration::generateProgramJava( const std::shared_ptr< ICodeGe
     myClass->add( method, ClassUnit::PROTECTED );
     return myClass->compile();
 }
-std::string ClassGeneration::generateProgramSharp( const std::shared_ptr< ICodeGenerator >& factory ) {
-    auto myClass = factory->addClass( "MyClass" );
+std::string ClassGeneration::generateProgramSharp() {
+    auto myClass = m_factory->addClass( "MyClass" );
     myClass->add( m_factory->addMethod( "testFunc1", "void", 0 ), ClassUnit::PUBLIC);
     myClass->add( m_factory->addMethod( "testFunc2", "void", MethodUnit::STATIC ), ClassUnit::PRIVATE);
     myClass->add( m_factory->addMethod( "testFunc3", "void", MethodUnit::STATIC), ClassUnit::PRIVATE_PROTECTED);
@@ -58,13 +58,13 @@ std::string ClassGeneration::Program()
 {
     switch (m_flag) {
     case CPP:
-        return generateProgramCpp(m_factory);
+        return generateProgramCpp();
         break;
     case JAVA:
-        return generateProgramJava(m_factory);
+        return generateProgramJava();
         break;
     case SHARP:
-        return generateProgramSharp(m_factory);
+        return generateProgramSharp();
         break;
     default:
         break;
